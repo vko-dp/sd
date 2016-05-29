97556be46a26a3552c31d0474b528915d1de97af
varenko oleg <vko-dp@mail.ru>
2016-05-29 18:45:33 +0300

    links

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,6 +36,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
+            ['label' => 'Товары', 'url' => ['/position/index']],
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
